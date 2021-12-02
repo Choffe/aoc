22d60594b59ee8f49796b212e17a9879f691cd14
Kristofer Jacobson <kristofj@axis.com>
2021-12-02 07:12:48 +0100

    formatting last commit
    Change-Id: I69ded878016586769df3e81b08da36948e7351e8

--- aoc21_rust/src/day1.rs
+++ aoc21_rust/src/day1.rs
@@ -9,13 +9,15 @@ pub fn run() {
 
 fn increasing_cnt(vector: Vec<i32>) -> i32 {
     vector.iter().zip(vector[1..].iter()).fold(
-        0, |acc, (first, second)| {
+        0,
+        |acc, (first, second)| {
             if first < second {
                 acc + 1
             } else {
                 acc
             }
-        })
+        },
+    )
 }
 
 fn part1(input: &str) -> i32 {
@@ -28,7 +30,7 @@ fn part2(input: &str) -> i32 {
     let s = fs::read_to_string(input).unwrap();
     let lines: Vec<i32> = s.trim().split('\n').map(|x| x.parse().unwrap()).collect();
     let mut windowed = vec![];
-    for i in 0..lines.len()-2 {
+    for i in 0..lines.len() - 2 {
         windowed.push(lines[i] + lines[i + 1] + lines[i + 2])
     }
     increasing_cnt(windowed)
